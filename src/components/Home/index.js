d8902f8f15caf52823c31f6a2e89103bc343a438
assalielmehdi@gmail.com <assalielmehdi@gmail.com>
2019-03-27 20:04:18 +0100

    Refactor project

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
-import Header from "../Header";
-import Footer from "../Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 class Home extends Component {
   componentWillMount() {
