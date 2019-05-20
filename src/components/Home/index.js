e1f525e29dac4e351e7a7a822f5fb004f406d6ab
widad boujaafar <boujaafar58@gmail.com>
2019-05-20 06:43:13 +0100

    add section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,6 +12,35 @@ class Home extends Component {
     return (
       <Fragment>
         <Header />
+        <div className=" view max-width">
+          <div id="section1" className="container-fluid">
+            <h1>Section 1</h1>
+            <p>
+              Nulla facilisi. Ut sit amet cursus dui , quis imperdiet dolor.
+              Duis non mollis augue. Sed elementum dolor vitae mauris
+              scelerisque posuere.
+            </p>
+            <p>
+              Nulla facilisi. Ut sit amet cursus dui , quis imperdiet dolor.
+              Duis non mollis augue. Sed elementum dolor vitae mauris
+              scelerisque posuere.
+            </p>
+          </div>
+          <div id="section2" className="container-fluid">
+            <h1>Section 2</h1>
+            <p>
+              Nulla facilisi. Ut sit amet cursus dui , quis imperdiet dolor.
+              Duis non mollis augue. Sed elementum dolor vitae mauris
+              scelerisque posuere.
+            </p>
+            <p>
+              Nulla facilisi. Ut sit amet cursus dui , quis imperdiet dolor.
+              Duis non mollis augue. Sed elementum dolor vitae mauris
+              scelerisque posuere.
+            </p>
+          </div>
+          <div />
+        </div>
         <Footer />
       </Fragment>
     );
